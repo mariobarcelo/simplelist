6f1ee6c61756b75df6248b835036b3e06140afb7
Mario Barceló <mariobarcelosospedra@gmail.com>
2023-05-29 15:26:58 +0200

    Add data persistence using LocalStorage

--- src/components/TodoApp/TodoApp.jsx
+++ src/components/TodoApp/TodoApp.jsx
@@ -4,8 +4,24 @@ import TodoForm from '../TodoForm';
 import TodoList from '../TodoList/TodoList';
 
 function TodoApp() {
-	const [itemsList, setItemsList] = React.useState([]);
+	const [itemsList, setItemsList] = React.useState(() => {
+		const storedItems = JSON.parse(localStorage.getItem('items'));
+		if (storedItems) {
+			return storedItems;
+		} else {
+			return [];
+		}
+	});
+
 	console.log('itemsList - TodoApp: ', itemsList);
+
+	React.useEffect(() => {
+		const storedItems = JSON.parse(localStorage.getItem('items'));
+
+		if (storedItems !== itemsList) {
+			localStorage.setItem('items', JSON.stringify(itemsList));
+		}
+	}, [itemsList]);
 
 	return (
 		<div className={styles.wrapper}>
